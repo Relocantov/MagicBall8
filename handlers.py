8e0ad9877e79a46e6d7dba4c1c0f9752c062321f
relocantov <balakinul@gmail.com>
2025-02-22 21:49:34 +0700

    Исправил регистрацию команд в aiogram 3.x

--- handlers.py
+++ handlers.py
@@ -1,7 +1,8 @@
 from aiogram import Router, types
+from aiogram.filters import Command
 
 router = Router()
 
-@router.message(commands=["start"])
+@router.message(Command("start"))
 async def start(message: types.Message):
     await message.answer("Привет! Я Telegram-бот!")
